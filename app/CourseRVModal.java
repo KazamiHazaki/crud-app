1a75ee05a2e881317bb038a3d6a675a70fa7b493
KazamiHazaki <alluka.zoldyck999@gmail.com>
2023-04-08 17:19:50 +0700

    Initial commit

---
+++ app/CourseRVModal.java
@@ -0,0 +1,2 @@
+public class CourseRVModal {
+}
---
+++ app/src/main/java/com/example/crud_app/User.java
@@ -0,0 +1,11 @@
+package com.example.crud_app;
+
+public class User {
+    public String username;
+    public String email;
+
+    public User(String username, String email){
+        this.username= username;
+        this.email= email;
+    }
+}
